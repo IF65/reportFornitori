473ede6b5fe1722a670197647c7e2008f72f1a82
Marco Gnecchi <marco.gnecchi@icloud.com>
2020-04-15 14:01:40 +0200

    allineamento

--- src/reportHuawei.php
+++ src/reportHuawei.php
@@ -141,12 +141,12 @@
 
         $mail->From = 'edp@supermedia.it';
         $mail->FromName = 'EDP Supermedia';
-        $mail->addAddress('marco.gnecchi@supermedia.it', 'Marco Gnecchi');     // Add a recipient
-        //$mail->addAddress('nicola.pirovano@supermedia.it', 'Nicola Pirovano');     // Add a recipient
-        //$mail->addAddress('sergio.guidi@supermedia.it', 'Sergio Guidi');     // Add a recipient
+        //$mail->addAddress('marco.gnecchi@supermedia.it', 'Marco Gnecchi');     // Add a recipient
+        $mail->addAddress('nicola.pirovano@supermedia.it', 'Nicola Pirovano');     // Add a recipient
+        $mail->addAddress('sergio.guidi@supermedia.it', 'Sergio Guidi');     // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         $mail->addReplyTo('marco.gnecchi@supermedia.it', 'Marco Gnecchi');
-        $mail->addCC('marco.gnecchi@supermedia.it');
+        $mail->addCC('marco.gnecchi@supermedia.it', 'Marco Gnecchi');
         //$mail->addBCC('bcc@example.com');
 
         $mail->WordWrap = 50;                                 // Set word wrap to 50 characters
